a84ff5949a4929a1db9380528b0adc87e49a7b35
Hari Shankar Singh <hari@lmg8.tech>
2025-08-24 08:56:27 +0530

    Add project files.

---
+++ Find Words Containing Character/Program.cs
@@ -0,0 +1,28 @@
+﻿public class program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(FindWordsContaining(["leet", "code"],'e'));// output [0,1]
+        Console.WriteLine(FindWordsContaining(["abc", "bcd", "aaaa", "cbc"], 'z'));// output []
+        Console.WriteLine(FindWordsContaining(["abc", "bcd", "aaaa", "cbc"], '2'));// output [0,2]
+    }
+    public static IList<int> FindWordsContaining(string[] words, char x)
+    {
+        IList<int> list = new List<int>();
+        for(int i=0; i<words.Length; i++)
+        {
+            string word = words[i];
+            // first approach
+           //var res= word.IndexOf(x);
+           //if(res >=0)
+           //     list.Add(i);
+
+            //second approach
+            if(word.Contains(x))
+            {
+                list.Add(i);
+            }
+        }
+        return list;
+    }
+}
